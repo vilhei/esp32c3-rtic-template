5a84609224d64ca4b179c4a99257ec1e2916d9d2
Ville Heikkinen <ville.heikkinen98@hotmail.fi>
2024-06-30 16:22:25 +0300

    Minimal example

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@
 #![feature(type_alias_impl_trait)]
 
 use rtic::app;
-mod timer_task;
 
 // remove dispatchers if you do not need software tasks. You need to have as many dispatchers as you have different priority levels for software tasks
-#[app(device=esp32c3, dispatchers=[FROM_CPU_INTR0, FROM_CPU_INTR1,  FROM_CPU_INTR2,  FROM_CPU_INTR3])]
+#[app(device=esp32c3, dispatchers=[FROM_CPU_INTR0])]
 mod app {
     use esp_backtrace as _; // Panic behaviour
 
@@ -15,7 +14,7 @@ mod app {
         clock::ClockControl,
         delay::Delay,
         gpio::{Gpio9, Input, PullUp},
-        peripherals::{Peripherals, TIMG0, TIMG1},
+        peripherals::{Peripherals, TIMG0},
         prelude::*,
         timer::{Timer, Timer0, TimerGroup},
         IO,
@@ -24,9 +23,7 @@ mod app {
 
     // Shared resources go here
     #[shared]
-    struct Shared {
-        timer1: Timer<Timer0<TIMG1>>,
-    }
+    struct Shared {}
 
     // Local resources go here
     #[local]
@@ -34,7 +31,6 @@ mod app {
         delay: Delay,
         timer0: Timer<Timer0<TIMG0>>,
         button: Gpio9<Input<PullUp>>,
-        timer1_is_running: bool,
     }
 
     #[init]
@@ -51,9 +47,6 @@ mod app {
         let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
         let mut timer0 = timer_group0.timer0;
 
-        let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
-        let timer1 = timer_group1.timer0;
-
         let mut button = io.pins.gpio9.into_pull_up_input();
         button.listen(esp_hal::gpio::Event::FallingEdge);
 
@@ -63,17 +56,16 @@ mod app {
         task1::spawn().expect("Failed to spawn task1");
 
         (
-            Shared { timer1 },
+            Shared {},
             Local {
                 delay,
                 timer0,
                 button,
-                timer1_is_running: false,
             },
         )
     }
 
-    // Optional idle, can be removed if not needed. Has prioty of 0
+    // Optional idle, can be removed if not needed. Has priority of 0
     #[idle(local = [delay])]
     fn idle(_: idle::Context) -> ! {
         loop {
@@ -83,63 +75,24 @@ mod app {
         }
     }
 
-    use crate::timer_task::{timer0_task, timer1_task};
-
     /// Tasks can be moved to different scope by using extern methods
-    extern "Rust" {
-        #[task(binds= TG0_T0_LEVEL, local=[timer0])]
-        fn timer0_task(cx: timer0_task::Context);
-
-        #[task(binds= TG1_T0_LEVEL, shared=[timer1])]
-        fn timer1_task(mut cx: timer1_task::Context);
+    #[task(binds= TG0_T0_LEVEL, local=[timer0])]
+    fn timer0_task(cx: timer0_task::Context) {
+        cx.local.timer0.clear_interrupt();
+        println!("Hello from timer0 task");
+        cx.local.timer0.start(3u64.secs());
     }
 
-    /// Sets timer1_task on and off via the boot button on the esp32c3
-    #[task(binds=GPIO, local=[button, timer1_is_running], shared=[timer1])]
-    fn button(mut cx: button::Context) {
+    /// Binds to boot button 
+    #[task(binds=GPIO, local=[button])]
+    fn button(cx: button::Context) {
         cx.local.button.clear_interrupt();
         println!("Button task");
-        cx.shared.timer1.lock(|t| {
-            *cx.local.timer1_is_running = match cx.local.timer1_is_running {
-                true => {
-                    t.clear_interrupt();
-                    t.unlisten();
-                    false
-                }
-                false => {
-                    t.clear_interrupt();
-                    t.listen();
-                    t.start(3u64.secs());
-                    true
-                }
-            }
-        })
     }
 
     // Software task 1
     #[task(priority = 1)]
     async fn task1(_cx: task1::Context) {
         println!("Hello from software task1");
-    }
-
-    #[task(priority = 1)]
-    async fn task1b(_cx: task1b::Context) {
-        println!("Hello from software task1b");
-    }
-
-    // Software task 2 with higher priority than task 1
-    #[task(priority = 2)]
-    async fn task2(_cx: task2::Context) {
-        println!("Hello from software task2");
-    }
-
-    #[task(priority = 3)]
-    async fn task3(_cx: task3::Context) {
-        println!("Hello from software task3");
-    }
-
-    #[task(priority = 4)]
-    async fn task4(_cx: task4::Context) {
-        println!("Hello from software task4");
     }
 }
